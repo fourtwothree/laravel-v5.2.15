466bd17af58bd86671b118a7f1b285a96c200586
fourtwothree <hxh15956201026@126.com>
2016-02-15 17:52:31 +0800

    老大亲自演示配置路由

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')->with('title', 'd45HE');
+});
+
+Route::get('/h', function () {
+    return 'why';
 });
 
 /*
